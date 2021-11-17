07a587517d1e8cd97c8bb79f5bd654e36a495de7
OmarElKhatibCS <omarelkhatib@mail.com>
2021-11-18 00:02:03 +0200

    add youtube link validation

--- main.go
+++ main.go
@@ -5,7 +5,12 @@ import (
 
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/validation"
 	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	ytExp = `http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?`
 )
 
 func main() {
@@ -17,7 +22,17 @@ func main() {
 
 	slider := widget.NewSlider(0, 100)
 
+	linkValidator := validation.NewRegexp(ytExp, "Invalid youtube link")
+
 	playBtn := widget.NewButton("play", func() {
+		if input.Text == "" {
+			log.Println("Empty link provided")
+			return
+		}
+		if err := linkValidator(input.Text); err != nil {
+			log.Println(err)
+			return
+		}
 		log.Println("Playing:", input.Text)
 	})
 
